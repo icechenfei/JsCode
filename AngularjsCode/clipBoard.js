6082b59551347844ce65e8922e28d49f55008e6f
icechenfei <624598042@qq.com>
2018-05-18 13:22:45 +0800

    This is Angularjs Code

---
+++ AngularjsCode/clipBoard.js
@@ -0,0 +1,31 @@
+angular.module('myTestModule')
+  .directive('clipBoard', clipBOard);
+
+  function clipBoard($injector){
+    return {
+      restrict: 'ACEM'.
+      scope: {
+        data: '=clipValue'
+      },
+      link: function(scope, element) {
+        var toastr = $injector.get('toastr');
+        var local  = $injector.get('Local');
+        element.on('mousedown', function(event){
+          var aux = document.createElement("input");
+          var content = scoep.data;
+          aux.setAttribute("value", content)
+          document.body.appendChild(aux);
+          aux.select()
+          try{
+            document.execCommand("copy");
+            toastr.success(local.lang("Copy Success"))
+          }
+          catch(err){
+            toastr.warning(local.lang("Please press Ctrl/Cmd+C to copy"))
+          }
+          document.body.removeChild(aux);
+        })
+      }
+    }
+  }
+})();
